4f9f5285288d9219483d6fd8a3b872388f922044
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-12 22:32:27 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -49,6 +49,6 @@ module tt_um_ULSR88 (
       //ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, dn 1'b0};
 
 endmodule
