18725869220c1d9b15991318a87e2f7a80371857
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-12 20:02:59 +0200

    Update test.py debug

--- test/test.py
+++ test/test.py
@@ -18,48 +18,46 @@ FAC    = 128  # out
 async def TestFullAdder(dut, f1, f2, f3, fs, fc):
   dut.uio_in.value = (f1*FA1) + (f2*FA2) + (f3*FA3)
   await ClockCycles(dut.clk, 3)
-  assert int(dut.uio_out.value[6]) == fs
-  assert int(dut.uio_out.value[7]) == fc
+  dut._log.info(str(f1)+str(f2)+str(f3)+" : "+str(dut.uio_out.value[6])+str(dut.uio_out.value[7]))
+#  assert int(dut.uio_out.value[6]) == fs
+#  assert int(dut.uio_out.value[7]) == fc
 
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
+  dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+  # Set the clock period to 10 us (100 KHz)
+  clock = Clock(dut.clk, 10, unit="us")
+  cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
+  # Reset
+  dut._log.info("Reset")
+  dut.ena.value = 1
+  dut.ui_in.value = 0
+  dut.uio_in.value = 0
+  dut.rst_n.value = 0
+  await ClockCycles(dut.clk, 10)
+  dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+  dut._log.info("Test project behavior")
 
-    await TestFullAdder(dut, 0, 0, 0, 0, 0)
-    await TestFullAdder(dut, 0, 0, 1, 1, 0)
-    await TestFullAdder(dut, 0, 1, 0, 1, 0)
-    await TestFullAdder(dut, 0, 1, 1, 0, 1)
-    await TestFullAdder(dut, 1, 0, 0, 1, 0)
-    await TestFullAdder(dut, 1, 0, 1, 0, 1)
-    await TestFullAdder(dut, 1, 1, 0, 0, 1)
-    await TestFullAdder(dut, 1, 1, 1, 1, 1)
+  await TestFullAdder(dut, 0, 0, 0, 0, 0)
+  await TestFullAdder(dut, 0, 0, 1, 1, 0)
+  await TestFullAdder(dut, 0, 1, 0, 1, 0)
+  await TestFullAdder(dut, 0, 1, 1, 0, 1)
+  await TestFullAdder(dut, 1, 0, 0, 1, 0)
+  await TestFullAdder(dut, 1, 0, 1, 0, 1)
+  await TestFullAdder(dut, 1, 1, 0, 0, 1)
+  await TestFullAdder(dut, 1, 1, 1, 1, 1)
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+  # Set the input values you want to test
+  dut.ui_in.value = 20
+  dut.uio_in.value = 30
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+  # Wait for one clock cycle to see the output values
+  await ClockCycles(dut.clk, 1)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
-
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+  # The following assersion is just an example of how to check the output values.
+  # Change it to match the actual expected output of your module:
+  assert dut.uo_out.value == 50
