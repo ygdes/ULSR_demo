a03acd4952811fd1e6049fceb9cc4f031b29f9af
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-12 19:39:36 +0200

    Update FullAdder_sg13.v

--- src/FullAdder_sg13.v
+++ src/FullAdder_sg13.v
@@ -9,11 +9,11 @@ module FullAdderSG13 (
 );
   wire t, u;
 
-  -- sum:
+  // sum:
   sg13_xor2_1 x1 (.X(t), .A(d1), .B(d2));
   sg13_xor2_1 x2 (.X(S), .A(t ), .B(d3));
 
-  -- carry out:
+  // carry out:
   sg13_a22oi_1 a(.A1(d1), .A2(d2), .B1(t), .B2(d3), .Y(u));
   sg13_inv_1 i(.A(u), .Y(C));
 endmodule
