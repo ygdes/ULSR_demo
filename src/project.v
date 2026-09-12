f8c0edbb3e7fd654f6659c655cd1fb960db5fc0c
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-12 18:09:01 +0200

    Update project.v

--- src/project.v
+++ src/project.v
@@ -16,10 +16,27 @@ module tt_um_ULSR88 (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+  wire SD, SC, DO,
+       FA1, FA2, FA3,
+       FAS, FAC;
+
+//  # Bidirectional pins
+//  uio[0]: "SD"
+//  uio[1]: "SC"
+//  uio[2]: "DO" => out
+//  uio[3]: "FA1"
+//  uio[4]: "FA2"
+//  uio[5]: "FA3"
+//  uio[6]: "FAC" => out
+//  uio[7]: "FAS" => out
+
+  assign uio_oe  = 8'b11000100;
+  assign uio_out = 0;
+
+
   // All output pins must be assigned. If not used, assign to 0.
   assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
