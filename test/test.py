4050ac001e148955fb8a5e04ed15dd94dfd93f4b
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-12 21:26:53 +0200

    Update test.py

--- test/test.py
+++ test/test.py
@@ -51,13 +51,13 @@ async def test_project(dut):
   await TestFullAdder(dut, 1, 1, 0, 0, 1)
   await TestFullAdder(dut, 1, 1, 1, 1, 1)
 
-  # Set the input values you want to test
-  dut.ui_in.value = 20
-  dut.uio_in.value = 30
+#  # Set the input values you want to test
+#  dut.ui_in.value = 20
+#  dut.uio_in.value = 30
 
-  # Wait for one clock cycle to see the output values
-  await ClockCycles(dut.clk, 1)
+#  # Wait for one clock cycle to see the output values
+#  await ClockCycles(dut.clk, 1)
 
-  # The following assersion is just an example of how to check the output values.
-  # Change it to match the actual expected output of your module:
-  assert dut.uo_out.value == 50
+#  # The following assersion is just an example of how to check the output values.
+#  # Change it to match the actual expected output of your module:
+#  assert dut.uo_out.value == 50
