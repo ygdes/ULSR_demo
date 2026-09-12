a00dda7379bc4575109529047c7136491105bb34
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-12 22:39:11 +0200

    Update project.v : "update" ???

--- src/project.v
+++ src/project.v
@@ -40,15 +40,15 @@ module tt_um_ULSR88 (
   FullAdderSG13 FA(.d1(FA1), .d2(FA2), .d3(FA3), .S(FAS), .C(FAC));
   assign uio_out = {FAC, FAS, 3'b000, DO, 2'b00};
 
-  wire Update, dn;
-  ULSR_input_ctrl(.SD(SD), .SC(SC), .Update(Update), .D(DO), .D_N(dn));
+  wire UpdateOut, dn;
+  ULSR_input_ctrl(.SD(SD), .SC(SC), .Update(UpdateOut), .D(DO), .D_N(dn));
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out = {7'b000 0000, Update};
+  assign uo_out = {7'b000 0000, UpdateOut};
 
       //ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, dn 1'b0};
+  wire _unused = &{ena, clk, rst_n, dn, 1'b0};
 
 endmodule
