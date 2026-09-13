84f5505b444da0b58c40a3665159174359e3efb4
YGDES <262647190+ygdes@users.noreply.github.com>
2026-09-13 02:14:07 +0200

    Update project.v : ULSR_out_bit x 8

--- src/project.v
+++ src/project.v
@@ -40,16 +40,27 @@ module tt_um_ULSR88 (
   FullAdderSG13 FA(.d1(FA1), .d2(FA2), .d3(FA3), .S(FAS), .C(FAC));
   assign uio_out = {FAC, FAS, 3'b000, DO, 2'b00};
 
-  wire UpdateStrobe, dn;
-  ULSR_input_ctrl ictl(.SD(SD), .SC(SC), .Update(UpdateStrobe), .D(DO), .D_N(dn));
+  wire UpdateStrobe;
+  wire [8:0] dp, dn;
+  ULSR_input_ctrl ictl(.SD(SD), .SC(SC), .Update(UpdateStrobe), .D(dp[0]), .D_N(dn[0]));
+  // output registers:
+  ULSR_out_bit ob0(.D(dp[0]), .D_N(dn[0]), .SD(SD), .SC(SC), .update(UpdateStrobe), .Dout(uo_out[1]), .Q(dp[1]), .Q_N(dn[1]));
+  ULSR_out_bit ob1(.D(dp[1]), .D_N(dn[1]), .SD(SD), .SC(SC), .update(UpdateStrobe), .Dout(uo_out[2]), .Q(dp[2]), .Q_N(dn[2]));
+  ULSR_out_bit ob2(.D(dp[2]), .D_N(dn[2]), .SD(SD), .SC(SC), .update(UpdateStrobe), .Dout(uo_out[3]), .Q(dp[3]), .Q_N(dn[3]));
+  ULSR_out_bit ob3(.D(dp[3]), .D_N(dn[3]), .SD(SD), .SC(SC), .update(UpdateStrobe), .Dout(uo_out[4]), .Q(dp[4]), .Q_N(dn[4]));
+  ULSR_out_bit ob4(.D(dp[4]), .D_N(dn[4]), .SD(SD), .SC(SC), .update(UpdateStrobe), .Dout(uo_out[5]), .Q(dp[5]), .Q_N(dn[5]));
+  ULSR_out_bit ob5(.D(dp[5]), .D_N(dn[5]), .SD(SD), .SC(SC), .update(UpdateStrobe), .Dout(uo_out[6]), .Q(dp[6]), .Q_N(dn[6]));
+  ULSR_out_bit ob6(.D(dp[6]), .D_N(dn[6]), .SD(SD), .SC(SC), .update(UpdateStrobe), .Dout(uo_out[7]), .Q(dp[7]), .Q_N(dn[7]));
+  ULSR_out_bit ob7(.D(dp[7]), .D_N(dn[7]), .SD(SD), .SC(SC), .update(UpdateStrobe), .Dout(uo_out[8]), .Q(dp[8]), .Q_N(dn[8]));
+
 
   wire CaptureStrobe;
   ULSR_output_ctrl octrl(.SD(SD), .SC(SC), .Capture(CaptureStrobe));
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out = {6'b000000, CaptureStrobe, UpdateStrobe};
+  //assign uo_out = {6'b000000, CaptureStrobe, UpdateStrobe};
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, dn, 1'b0};
+    wire _unused = &{ena, clk, rst_n, dn,  CaptureStrobe, 1'b0};
 
 endmodule
